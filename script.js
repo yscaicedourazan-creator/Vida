685630625c499b602e1b59291a6445ec269cd996
yscaicedourazan-creator <yscaicedourazan@gmail.com>
2025-10-18 09:56:12 -0500

    Agregando Musica

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ function checkPassword() {
   if (clave === "1824") {
     document.getElementById("access").style.display = "none";
     document.getElementById("content").style.display = "block";
+    //Reproducir Musica //
+    const audio = document.getElementById("musica");
+    audio.play();
+
   } else {
     alert("Clave incorrecta. Intenta de nuevo.");
   }
